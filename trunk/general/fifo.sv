7c103b5d9cc0405e7466048385a122436355f9d7
ericdellis <ericdellis@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-05 16:00:21 +0000

    updated fifo

--- trunk/general/fifo.sv
+++ trunk/general/fifo.sv
@@ -12,7 +12,8 @@ module fifo #(
   input pop_req,
   input [WIDTH-1:0] data_in,
   output full,
-  output [WIDTH-1:0] data_out
+  output [WIDTH-1:0] data_out,
+  output [WIDTH-1:0] peek
 );
 
 wire empty;
@@ -21,11 +22,23 @@ wire half_full;
 wire almost_full;
 wire error;
 
+reg [WIDTH-1:0] peek_data;
+
 DW_fifo_s1_sf #(WIDTH, depth, ae_level, af_level, err_mode, rst_mode)
 	buffer (.clk(clk), .rst_n(rst), .push_req_n(push_req), .diag_n(1'b1),
 	.data_in(data_in), .empty(empty), .almost_empty(almost_empty),
 	.half_full(half_full), .almost_full(almost_full), .full(full),
 	.error(error), .data_out(data_out) );
 
+always @ (posedge clk) begin
+  if (!rst) begin
+    peek_data <= 0;
+  end
+  else if (!full) begin
+    peek_data <= data_in;
+  end
+end
+
+assign peek = peek_data;
 
 endmodule
