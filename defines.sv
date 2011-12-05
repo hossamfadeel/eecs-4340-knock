3eb5df7ba01420ad37c05d00626bb89e49c12c1b
zsalzbank@gmail.com <zsalzbank@gmail.com@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-05 04:43:29 +0000

    fixed for bigger nocs

--- defines.sv
+++ defines.sv
@@ -1,4 +1,6 @@
+`define NSMO (`NOC_SIZE-1) 
+
 `define TOP(y) y==0
-`define BOTTOM(y) y==(`NOC_SIZE-1)
+`define BOTTOM(y) y==`NSMO
 `define LEFT(x) x==0
-`define RIGHT(x) x==(`NOC_SIZE-1)
+`define RIGHT(x) x==`NSMO
--- dut/noc.sv
+++ dut/noc.sv
@@ -4,30 +4,32 @@ module noc(
 	node_interface.dut local_node [1:`INTERFACES] 
 );
 
-  node_interface c [1:24] (.clk(clk.clk));
+  node_interface c [1:(2*`NOC_SIZE*`NOC_SIZE - 2*`NOC_SIZE)] (.clk(clk.clk));
 
   generate
     for(genvar x = 0; x < `NOC_SIZE; x = x + 1) begin
       for(genvar y = 0; y < `NOC_SIZE; y = y + 1) begin
+        for(genvar i=x+y*`NOC_SIZE+1; i>0; i=-1) begin
         if(`TOP(y) && `LEFT(x)) begin
-          node3 tl (.clk(clk), .reset(reset), .local_node(local_node[x + y*`NOC_SIZE + 1]),  .node_0(c[1]), .node_1(c[`NOC_SIZE]));
+          node3 tl (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[1]), .node_1(c[`NOC_SIZE]));
         end else if (`TOP(y) && `RIGHT(x)) begin
-          node3 tr (.clk(clk), .reset(reset), .local_node(local_node[x + y*`NOC_SIZE + 1]),  .node_0(c[`NOC_SIZE*2 - 1]), .node_1(c[`NOC_SIZE - 1]));
+          node3 tr (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[`NOC_SIZE*2 - 1]), .node_1(c[`NOC_SIZE - 1]));
         end else if (`BOTTOM(y) && `LEFT(x)) begin
-          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[x + y*`NOC_SIZE + 1]),  .node_0(c[x + y*`NOC_SIZE + 1 + (`NOC_SIZE-1)*y]), .node_1(c[(x + y*`NOC_SIZE + 1) + (`NOC_SIZE-1)*(y-1) - 1]));
+          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i + `NSMO*y]), .node_1(c[i + `NSMO*(y-1) - 1]));
         end else if (`BOTTOM(y) && `RIGHT(x)) begin
-          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[x + y*`NOC_SIZE + 1]),  .node_0(c[`INTERFACES]), .node_1(c[(x + y*`NOC_SIZE + 1) + (`NOC_SIZE-1)*(y-1) - 1]));
+          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[`INTERFACES]), .node_1(c[i + `NSMO*(y-1) - 1]));
         end else if (`TOP(y)) begin
-          node4 t (.clk(clk), .reset(reset), .local_node(local_node[x+y*`NOC_SIZE+1]),  .node_0(c[x+y*`NOC_SIZE+1]), .node_1(c[x+y*`NOC_SIZE+1+`NOC_SIZE-1]), .node_2(c[x+y*`NOC_SIZE+1-1]));
+          node4 t (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i]), .node_1(c[i+`NOC_SIZE-1]), .node_2(c[i-1]));
         end else if (`BOTTOM(y)) begin
-          node4 b (.clk(clk), .reset(reset), .local_node(local_node[x+y*`NOC_SIZE+1]),  .node_0(c[x + y*`NOC_SIZE + 1 + (`NOC_SIZE-1)*y]), .node_1(c[x+y*`NOC_SIZE+1+(`NOC_SIZE-1)*y-1]), .node_2(c[(x + y*`NOC_SIZE + 1) + (`NOC_SIZE-1)*(y-1) - 1]));
+          node4 b (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i + `NSMO*y]), .node_1(c[i+`NSMO*y-1]), .node_2(c[i + `NSMO*(y-1) - 1]));
         end else if (`LEFT(x)) begin
-          node4 l (.clk(clk), .reset(reset), .local_node(local_node[x+y*`NOC_SIZE+1]),  .node_0(c[x+y*`NOC_SIZE+1+(`NOC_SIZE-1)*y]), .node_1(c[x+y*`NOC_SIZE+1+(`NOC_SIZE-1)*(y+1)]), .node_2(c[(x + y*`NOC_SIZE + 1) + (`NOC_SIZE-1)*(y-1) - 1]));
+          node4 l (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i+`NSMO*y]), .node_1(c[i+`NSMO*(y+1)]), .node_2(c[i + `NSMO*(y-1) - 1]));
         end else if (`RIGHT(x)) begin
-          node4 r (.clk(clk), .reset(reset), .local_node(local_node[x+y*`NOC_SIZE+1]),  .node_0(c[x+y*`NOC_SIZE+1+(`NOC_SIZE-1)*(y+1)]), .node_1(c[x+y*`NOC_SIZE+1+(`NOC_SIZE-1)*y-1]), .node_2(c[(x + y*`NOC_SIZE + 1) + (`NOC_SIZE-1)*(y-1) - 1]));
+          node4 r (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i+`NSMO*(y+1)]), .node_1(c[i+`NSMO*y-1]), .node_2(c[i + `NSMO*(y-1) - 1]));
         end else begin
-          node5 n (.clk(clk), .reset(reset), .local_node(local_node[x+y*`NOC_SIZE+1]),  .node_0(c[x+y*`NOC_SIZE+1 + y*(`NOC_SIZE-1)]), .node_1(c[x+y*`NOC_SIZE+1 + (y+1)*(`NOC_SIZE-1)]), .node_2(c[x+y*`NOC_SIZE+1+y*(`NOC_SIZE-1)-1]), .node_3(c[x+y*`NOC_SIZE+1+(y-1)*(`NOC_SIZE-1)-1]));
+          node5 n (.clk(clk), .reset(reset), .local_node(local_node[i]),  .node_0(c[i + y*`NSMO]), .node_1(c[i + (y+1)*`NSMO]), .node_2(c[i+y*`NSMO-1]), .node_3(c[i+(y-1)*`NSMO-1]));
         end
+      end
       end
     end
   endgenerate
