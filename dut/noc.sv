d8a4d04e2e3b80d9b4706343b81c68a25486d07b
zsalzbank@gmail.com <zsalzbank@gmail.com@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-06 16:25:30 +0000

    sim_nodes capture output of neighbors for processing

--- dut/noc.sv
+++ dut/noc.sv
@@ -9,26 +9,24 @@ module noc(
   generate
     for(genvar x = 0; x < `NOC_SIZE; x = x+1) begin
       for(genvar y = 0; y < `NOC_SIZE; y = y+1) begin
-        for(genvar i=x+y*`NOC_SIZE+1; i>0; i=-1) begin
-          if(`TOP(y) && `LEFT(x)) begin
-            node3 tl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]));
-          end else if (`TOP(y) && `RIGHT(x)) begin
-            node3 tr (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`SOUTH(x,y)]), .node_1(c[`WEST(x,y)]));
-          end else if (`BOTTOM(y) && `LEFT(x)) begin
-            node3 bl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`NORTH(x,y)]));
-          end else if (`BOTTOM(y) && `RIGHT(x)) begin
-            node3 bl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`WEST(x,y)]), .node_1(c[`NORTH(x,y)]));
-          end else if (`TOP(y)) begin
-            node4 t (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`WEST(x,y)]));
-          end else if (`BOTTOM(y)) begin
-            node4 b (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`WEST(x,y)]), .node_2(c[`NORTH(x,y)]));
-          end else if (`LEFT(x)) begin
-            node4 l (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`NORTH(x,y)]));
-          end else if (`RIGHT(x)) begin
-            node4 r (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`SOUTH(x,y)]), .node_1(c[`WEST(x,y)]), .node_2(c[`NORTH(x,y)]));
-          end else begin
-            node5 n (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`WEST(x,y)]), .node_3(c[`NORTH(x,y)]));
-          end
+        if(`TOP(y) && `LEFT(x)) begin
+          node3 tl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]));
+        end else if (`TOP(y) && `RIGHT(x)) begin
+          node3 tr (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`SOUTH(x,y)]), .node_1(c[`WEST(x,y)]));
+        end else if (`BOTTOM(y) && `LEFT(x)) begin
+          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`NORTH(x,y)]));
+        end else if (`BOTTOM(y) && `RIGHT(x)) begin
+          node3 bl (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`WEST(x,y)]), .node_1(c[`NORTH(x,y)]));
+        end else if (`TOP(y)) begin
+          node4 t (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`WEST(x,y)]));
+        end else if (`BOTTOM(y)) begin
+          node4 b (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`WEST(x,y)]), .node_2(c[`NORTH(x,y)]));
+        end else if (`LEFT(x)) begin
+          node4 l (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`NORTH(x,y)]));
+        end else if (`RIGHT(x)) begin
+          node4 r (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`SOUTH(x,y)]), .node_1(c[`WEST(x,y)]), .node_2(c[`NORTH(x,y)]));
+        end else begin
+          node5 n (.clk(clk), .reset(reset), .local_node(local_node[`INDEX(x,y)]),  .node_0(c[`EAST(x,y)]), .node_1(c[`SOUTH(x,y)]), .node_2(c[`WEST(x,y)]), .node_3(c[`NORTH(x,y)]));
         end
       end
     end
