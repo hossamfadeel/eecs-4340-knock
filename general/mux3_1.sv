1d7119462f4c0830cedb13e8fb67ffe35e03b083
ericdellis <ericdellis@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-08 21:46:00 +0000

    adding 2-1 and 3-1 one-hot muxes to general/

---
+++ general/mux3_1.sv
@@ -0,0 +1,23 @@
+module mux #(
+  parameter WIDTH = 16
+)
+(
+  input [WIDTH-1:0] data0,
+  input [WIDTH-1:0] data1,
+  input [WIDTH-1:0] data2,
+
+  input select0, select1, select2,
+  output logic [WIDTH-1:0] data_o
+
+);
+
+always begin
+  case({select2,select1,select0})
+    4'b100: data_o = data2;
+    4'b010: data_o = data1;
+    4'b001: data_o = data0;
+    default: data_o = 16'b0;
+  endcase
+end
+
+endmodule
