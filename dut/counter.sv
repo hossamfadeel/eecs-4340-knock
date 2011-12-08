104baa494609fee1ea94de2b7c016d5b92e86453
kevinkuo1214 <kevinkuo1214@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-08 12:25:14 +0000

    upload working counter

--- dut/counter.sv
+++ dut/counter.sv
@@ -2,43 +2,51 @@ module counter#(parameter ADD_WIDTH = 5)
 (
 	input clk,
 	input reset,
-	input [ADD_WIDTH-1:0] flip_length,
+	input [ADD_WIDTH-1:0] flit_length,
 	input receiving_data,
-	output count_done_o;
+	output logic count_done_o,
+	//output logic [ADD_WIDTH-1:0] cc_o
 );
 
 
-wire [ADD_WIDTH-1:0] next_count;
+logic [ADD_WIDTH-1:0] next_count;
 wire [ADD_WIDTH-1:0] current_count;
 
 always_comb begin
 	if(reset) begin
-			next_count = {ADD_WIDTH-1{0}};
+			next_count = 0;
 		end
 	else if(receiving_data == 1'b1) 
 		begin
-			next_count = flip_length;
+			next_count = flit_length;
 		end
-	else
+	else if(current_count != 0)
 		begin
 			next_count = current_count - 1'b1;
 		end
+
 
 	if(current_count == 0)
 		begin
 			count_done_o = 1'b1;
 		end
+	else
+		begin
+			count_done_o =1'b0;
+		end
+
+
 
 end
 
 
+//assign cc_o = current_count;
 
 
-
-register ff	#(.BITS(ADD_WIDTH))(
+register #(.BITS(ADD_WIDTH)) fffg (
 			//input
 			.clk(clk),
-                        .enable_i(1'b1),
+      .enable_i(1'b1),
 			.data_i(next_count),
 			.reset(reset),
 			
