6e5fb09e6959f8b0e5c0f61ce8a5d2e108ed3789
soselar@gmail.com <soselar@gmail.com@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-11 21:46:55 +0000

    controllers

--- dut/node5.sv
+++ dut/node5.sv
@@ -13,20 +13,33 @@ module node5 #(
 );
   parameter NUM_INTERFACES = 5;
 
-  wire buffer_full_out[1:NUM_INTERFACES], sending_data[1:NUM_INTERFACES];
-  wire [15:0] data_out[1:NUM_INTERFACES];
-  wire buffer_full_in[1:NUM_INTERFACES], receiving_data[1:NUM_INTERFACES];
-  wire [15:0] data_in[1:NUM_INTERFACES];
-  wire data_valid[1:NUM_INTERFACES];
+  wire buffer_full_out[NUM_INTERFACES-1:0], sending_data[NUM_INTERFACES-1:0];
+  wire [15:0] data_out[NUM_INTERFACES-1:0];
+  wire buffer_full_in[NUM_INTERFACES-1:0], receiving_data[NUM_INTERFACES-1:0];
+  wire [15:0] data_in[NUM_INTERFACES-1:0];
+  wire data_valid[NUM_INTERFACES-1:0];
+  wire [7:0] local_addr;
 
-  converter c1 (local_node, buffer_full_out[1], sending_data[1], data_out[1], buffer_full_in[1], receiving_data[1], data_in[1]);
-  converter c2 (node_0, buffer_full_out[2], sending_data[2], data_out[2], buffer_full_in[2], receiving_data[2], data_in[2]);
-  converter c3 (node_1, buffer_full_out[3], sending_data[3], data_out[3], buffer_full_in[3], receiving_data[3], data_in[3]);
-  converter c4 (node_2, buffer_full_out[4], sending_data[4], data_out[4], buffer_full_in[4], receiving_data[4], data_in[4]);
-  converter c5 (node_3, buffer_full_out[5], sending_data[5], data_out[5], buffer_full_in[5], receiving_data[5], data_in[5]);
+  wire [1:0] grant_0;
+  wire [1:0] grant_1;
+  wire [3:0] grant_2;
+  wire [3:0] grant_3;
+  wire [3:0] grant_4;
+  wire [4:0] grant_v;
+
+  wire [7:0] packet_addr [4:0];
+
+  assign local_addr[7:4] = NODE_Y[3:0];
+  assign local_addr[3:0] = NODE_X[3:0];
+
+  converter c0 (node_0, buffer_full_out[0], sending_data[0], data_out[0], buffer_full_in[0], receiving_data[0], data_in[0]);
+  converter c1 (node_1, buffer_full_out[1], sending_data[1], data_out[1], buffer_full_in[1], receiving_data[1], data_in[1]);
+  converter c2 (node_2, buffer_full_out[2], sending_data[2], data_out[2], buffer_full_in[2], receiving_data[2], data_in[2]);
+  converter c3 (node_3, buffer_full_out[3], sending_data[3], data_out[3], buffer_full_in[3], receiving_data[3], data_in[3]);
+  converter c4 (local_node, buffer_full_out[4], sending_data[4], data_out[4], buffer_full_in[4], receiving_data[4], data_in[4]);
 
   generate
-    for(genvar i = 1; i <= NUM_INTERFACES; i = i + 1) begin
+    for(genvar i = 0; i <= NUM_INTERFACES-1; i = i + 1) begin
       assign sending_data[i] = !buffer_full_in[i] & data_valid[i];
 
       fifo buffer ( .clk(clk.clk),
@@ -38,6 +51,36 @@ module node5 #(
                     .data_valid(data_valid[i]),
                     .data_out(data_out[i])
                   );
+
+
+      counter c ( .clk(clk.clk),
+                  .reset(reset.reset),
+                  .flit_length(8'h01),
+                  .count_enable(receiving_data[i]),
+                  .is_address(address_write_enable[i])
+                );
+
+      address_unit au ( .clk(clk.clk),
+                        .reset(reset.reset),
+                        .write_enable(address_write_enable[i]),
+                        .interface_data_in(data_in([i][7:0])),
+						.buffer_data_in(data_out[i][7:0]),
+						.data_select(data_valid[i]),
+                        .data_out(packet_addr[i])
+                      );
     end
   endgenerate
+
+  controller5 ctrl5(//input
+ 					.clk(clk.clk), 
+					.rst(reset.reset),
+					.packet_addr, 
+					.local_addr, 
+					.buffer_full_in,
+					.packet_valid(data_valid),
+					//output
+					.grant_0, .grant_1, .grant_2, .grant_3, .grant_4, .grant_v 
+					);
+
+
 endmodule
