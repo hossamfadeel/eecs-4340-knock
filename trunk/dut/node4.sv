8b3d8fbeec0ed621d3d7befa13d140eb94722d08
soselar@gmail.com <soselar@gmail.com@fa1445bf-cf37-49f0-ab72-e5f7236e6f76>
2011-12-13 19:40:57 +0000

    nodes

--- trunk/dut/node4.sv
+++ trunk/dut/node4.sv
@@ -24,6 +24,13 @@ module node4 #(
   wire [2:0] grant_3;
   wire [3:0] grant_v;
 
+  wire [7:0] packet_addr [4:0];
+
+  assign local_addr[7:4] = NODE_Y[3:0];
+  assign local_addr[3:0] = NODE_X[3:0];
+
+  wire [16:0] buffer_data_out [3:0];
+
   converter c0 (node_0, buffer_full_out[0], sending_data[0], data_out[0], buffer_full_in[0], receiving_data[0], data_in[0]);
   converter c1 (node_1, buffer_full_out[1], sending_data[1], data_out[1], buffer_full_in[1], receiving_data[1], data_in[1]);
   converter c2 (node_2, buffer_full_out[2], sending_data[2], data_out[2], buffer_full_in[2], receiving_data[2], data_in[2]);
@@ -40,27 +47,25 @@ module node4 #(
                     .data_in(data_in[i]),
                     .full(buffer_full_out[i]),
                     .data_valid(data_valid[i]),
-                    .data_out(data_out[i])
+                    .data_out(buffer_data_out[i])
                   );
 
-      counter c ( .clk(clk.clk),
-                  .reset(reset.reset),
-                  .flit_length(8'h01),
-                  .count_enable(receiving_data[i]),
-                  .is_address(address_write_enable[i])
-                );
+	  address_counter addr(
+        			.clk(clk.clk),
+                    .rst(reset.reset),
+       				.interface_flit_length(data_in[i][15:8]),
+       				.buffer_flit_length(buffer_data_out[i][15:8]),
+					.buffer_data_valid(data_valid[i]),
+					.interface_flit_address(data_in[i][7:0]),
+					.buffer_flit_address(buffer_data_out[i][7:0]),        
+      		 		.buffer_pop(sending_data[i]),
+      				.receiving_data(receiving_data[i]),
 
-      address_unit au ( .clk(clk.clk),
-                        .reset(reset.reset),
-                        .write_enable(address_write_enable[i]),
-                        .interface_data_in(data_in([i][7:0])),
-						.buffer_data_in(data_out[i][7:0]),
-						.data_select(data_valid[i]),
-                        .data_out(packet_addr[i])
-                      );
+					.flit_address_o(packet_addr),
+					);
     end
 
-	if (`TOP(NODE_Y))
+	if (`TOP(NODE_Y)) begin
 		controller4_edge_n (.clk(clk.clk),
                         .reset(reset.reset),
 						.packet_addr,
@@ -73,7 +78,40 @@ module node4 #(
 						.grant_v
 						);
 
-    else if (`BOTTOM(NODE_Y))
+		assign data_out[0] = sengding_data[0] ? buffer_data_out[0] : 16'b0;
+
+		MUX_3 mux_e(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[2]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_1[0]),
+				.select1(grant_1[1]),
+				.select2(grant_1[2]),
+				.data_o(data_out[1])
+		);
+
+		MUX_3 mux_w(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_2[0]),
+				.select1(grant_2[1]),
+				.select2(grant_2[2]),
+				.data_o(data_out[2])
+		);
+
+		MUX_3 mux_l(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[2]),
+				.select0(grant_3[0]),
+				.select1(grant_3[1]),
+				.select2(grant_3[2]),
+				.data_o(data_out[3])
+		);
+	end
+
+    else if (`BOTTOM(NODE_Y)) begin
 		controller4_edge_s (.clk(clk.clk),
                         .reset(reset.reset),
 						.packet_addr,
@@ -86,7 +124,40 @@ module node4 #(
 						.grant_v
 						); 
 
-	else if (`RIGHT(NODE_X))
+		assign data_out[0] = sengding_data[0] ? buffer_data_out[0] : 16'b0;
+
+		MUX_3 mux_e(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[2]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_1[0]),
+				.select1(grant_1[1]),
+				.select2(grant_1[2]),
+				.data_o(data_out[1])
+		);
+
+		MUX_3 mux_w(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_2[0]),
+				.select1(grant_2[1]),
+				.select2(grant_2[2]),
+				.data_o(data_out[2])
+		);
+
+		MUX_3 mux_l(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[2]),
+				.select0(grant_3[0]),
+				.select1(grant_3[1]),
+				.select2(grant_3[2]),
+				.data_o(data_out[3])
+		);
+	end
+
+	else if (`RIGHT(NODE_X)) begin
 		controller4_edge_e (.clk(clk.clk),
                         .reset(reset.reset),
 						.packet_addr,
@@ -100,7 +171,45 @@ module node4 #(
 						.grant_v
 						); 
 
-	else
+		MUX_2 mux_n(
+				.data0(buffer_data_out[1]),
+				.data1(buffer_data_out[3]),
+				.select0(grant_0[0]),
+				.select1(grant_0[1]),
+				.data_o(data_out[0])
+		);
+
+		MUX_2 mux_s(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[3]),
+				.select0(grant_1[0]),
+				.select1(grant_1[1]),
+				.data_o(data_out[1])
+		);
+
+
+		MUX_3 mux_w(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_2[0]),
+				.select1(grant_2[1]),
+				.select2(grant_2[2]),
+				.data_o(data_out[2])
+		);
+
+		MUX_3 mux_l(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[2]),
+				.select0(grant_3[0]),
+				.select1(grant_3[1]),
+				.select2(grant_3[2]),
+				.data_o(data_out[3])
+		);
+	end
+
+	else begin
 		controller4_edge_w (.clk(clk.clk),
                         .reset(reset.reset),
 						.packet_addr,
@@ -113,5 +222,44 @@ module node4 #(
 						.grant_3,
 						.grant_v
 						); 
+
+		MUX_2 mux_n(
+				.data0(buffer_data_out[1]),
+				.data1(buffer_data_out[3]),
+				.select0(grant_0[0]),
+				.select1(grant_0[1]),
+				.data_o(data_out[0])
+		);
+
+		MUX_2 mux_s(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[3]),
+				.select0(grant_1[0]),
+				.select1(grant_1[1]),
+				.data_o(data_out[1])
+		);
+
+
+		MUX_3 mux_e(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[3]),
+				.select0(grant_2[0]),
+				.select1(grant_2[1]),
+				.select2(grant_2[2]),
+				.data_o(data_out[2])
+		);
+
+		MUX_3 mux_l(
+				.data0(buffer_data_out[0]),
+				.data1(buffer_data_out[1]),
+				.data2(buffer_data_out[2]),
+				.select0(grant_3[0]),
+				.select1(grant_3[1]),
+				.select2(grant_3[2]),
+				.data_o(data_out[3])
+		);
+	end
+
   endgenerate
 endmodule
